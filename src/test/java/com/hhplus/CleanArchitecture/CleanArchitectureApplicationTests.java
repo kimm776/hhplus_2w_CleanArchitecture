8d1a45891e6673de966e2704008d40df64fc726c
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-10-01 01:53:13 +0900

    init

---
+++ src/test/java/com/hhplus/CleanArchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.CleanArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
